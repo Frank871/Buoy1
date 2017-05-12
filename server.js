b05a9ae001c073c722df003e7a7afa54edf13abd
Franklin Segura <franklinsegura@Franklins-MacBook-Air.local>
2017-05-12 12:51:23 -0400

    new update

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+var PORT = process.env.PORT || 3000;
 
-// Tells node that we are creating an "express" server
 var app = express();
 var db = require("./models");
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8081;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
 
-// BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// Import routes and give the server access to them.
+var routes = require("./controllers/poolguys_controller.js");
+require("./routes/apiroutes.js") (app);
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
-// ==============================================================================
-// LISTENER
-// The below code effectively "starts" our server
-// ==============================================================================
+app.use("/", routes);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
